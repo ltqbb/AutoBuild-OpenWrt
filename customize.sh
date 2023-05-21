75796472cae7efc697902a1e4c6da3196c2b4b57
ltqbb <125167546+ltqbb@users.noreply.github.com>
2023-05-21 17:57:04 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -20,4 +20,4 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #5. 修改主机名字，把ImmortalWrt修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/MlhKWrt/BrianRT_$(TZ=UTC-8 date "+%Y%m%d")/g' package/base-files/files/bin/config_generate
+sed -i 's/MlhkWrt/BrianRT_$(TZ=UTC-8 date "+%Y%m%d")/g' package/base-files/files/bin/config_generate
