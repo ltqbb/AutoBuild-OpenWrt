714f7dcae50ea3f7a8ee24620bcc825f2b83bf44
ltqbb <125167546+ltqbb@users.noreply.github.com>
2023-05-21 19:06:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,18 +6,21 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.100.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
+#2. name
+sed -i "s/hostname='.*'/hostname='MlhkWrt'/g" ./package/base-files/files/bin/config_generate
+
+#3. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
+#4. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 
-#4. Add a feed source
+#5. Add a feed source
 echo "src-git danshui https://github.com/281677160/openwrt-package" >> ./feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-#5. name
+#6. name
 # sed -i 's/MlhkWrt/BrianRT_$(TZ=UTC-8 date "+%Y%m%d")/g' package/base-files/files/bin/config_generate
