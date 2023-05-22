ef5053801a15fecd8dfd4df1dc79e3c8a0d07de8
ltqbb <125167546+ltqbb@users.noreply.github.com>
2023-05-22 11:49:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,10 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.100.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. name
-sed -i "s/hostname='.*'/hostname='MlhkWrt'/g" ./package/base-files/files/bin/config_generate
+sed -i "s/hostname='MlhkWrt'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
 
 #3. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
